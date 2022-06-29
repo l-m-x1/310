09255af8f1d46711fbf14945aaaf1a5291f494cb
Casey219 <2414996036@qq.com>
2022-06-29 08:52:28 +0800

    add mappers of Style and Trends

---
+++ MySpace/src/main/java/com/space/mapper/StyleMapper.java
@@ -0,0 +1,28 @@
+package com.space.mapper;
+
+import com.space.pojo.Style;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+public interface StyleMapper {
+
+    @Select("select * from style")
+    List<Style> selectAll();
+
+    @Select("select * from style where id=#{id}")
+    Style selectById(Integer id);
+
+    @Select("select * from style where uid=#{uid}")
+    List<Style> selectByUid(Integer uid);
+
+    @Insert("insert into style (type, uid) values(#{type},#{id})")
+    void insert(Style style);
+
+    @Delete("delete from  style where id=#{id}")
+    void deleteById(Integer id);
+
+    @Update("update style set type=#{type} where id=#{id}")
+    void updateByType(@Param("id") Integer id, @Param("type") Integer type);
+
+}
---
+++ MySpace/src/main/java/com/space/mapper/TrendsMapper.java
@@ -0,0 +1,30 @@
+package com.space.mapper;
+
+import com.space.pojo.Trends;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+public interface TrendsMapper {
+
+    @Select("select * from trends")
+    List<Trends>  selectAll();
+
+    @Select("select * from trends where id=#{id}")
+    Trends selectById(Integer id);
+
+    @Select("select * from trends where uid=#{uid}")
+    List<Trends> selectByUid(Integer uid);
+
+    @Insert("insert into trends(content,uid) values (#{content},#{uid})")
+    void insert(Trends trends);
+
+    @Delete("delete from trends where id=#{id}")
+    void deleteById(Integer id);
+
+    @Delete("delete from trends where uid=#{uid}")
+    void deleteByUid(Integer uid);
+
+    @Update("update trends set likes = #{likes} where id=#{id} ;")
+    void update(@Param("id") Integer id, @Param("likes") Integer likes);
+}
