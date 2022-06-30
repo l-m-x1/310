810374b9b38a588b83bf879d9c11e6d9d98c9043
aihei <3361040032@qq.com>
2022-06-30 16:55:13 +0800

    add service

--- MySpace/src/main/java/com/space/web/servlet/RegisterServlet.java
+++ MySpace/src/main/java/com/space/web/servlet/RegisterServlet.java
@@ -14,30 +14,34 @@ import java.io.IOException;
 
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends BaseServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        String username = request.getParameter("username");
-//        String password = request.getParameter("password");
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+////        String username = request.getParameter("username");
+////        String password = request.getParameter("password");
+//
+//        String username = jsonObject.getString("name");
+//        String password=jsonObject.getString("password");
+//
+//
+//        User user=new User();
+//        user.setPassword(password);
+//        user.setUsername(username);
+//        UserServiceImpl userService=new UserServiceImpl();
+//        userService.insert(user);
+//        //获取新建用户的账号
+//        Integer account = userService.getMaxId();
+//
+//        response.getWriter().write(account);
+//
+//
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        doGet(request, response);
+//    }
 
-        String username = jsonObject.getString("name");
-        String password=jsonObject.getString("password");
-
-
-        User user=new User();
-        user.setPassword(password);
-        user.setUsername(username);
-        UserServiceImpl userService=new UserServiceImpl();
-        userService.insert(user);
-        //获取新建用户的账号
-        Integer account = userService.getMaxId();
-
-        response.getWriter().write(account);
-
-
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+    public void register(){
+        System.out.println("hello");
     }
 }
